25d910a381c81b48ad85757f908c709c6ed539b7
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-08-12 19:46:33 +0900

    add testWithLabel

--- Tests/Library1Tests/Library1Tests.swift
+++ Tests/Library1Tests/Library1Tests.swift
@@ -14,8 +14,36 @@ func name(name: String) {
   #expect(name.starts(with: regex))
 }
 
-@Test(arguments: [("Name1", 1), ("Name2", 2), ("Name3", 3)])
+@Test(arguments: [
+  ("Name1", 1),
+  ("Name2", 2),
+  ("Name3", 3)
+])
 func name(name: String, age: Int) throws {
+  let captureAge = Reference<Int>()
+  
+  let regex = Regex {
+    Anchor.startOfLine
+    "Name"
+    TryCapture(as: captureAge) {
+      OneOrMore(.digit)
+    } transform: {
+      Int($0)
+    }
+    Anchor.endOfLine
+  }
+  
+  let metch = try #require(name.wholeMatch(of: regex))
+
+  #expect(metch[captureAge] == age)
+}
+
+@Test(arguments: [
+  (name: "Name1", age :1),
+  (name: "Name2", age: 2),
+  (name: "Name3", age: 3)
+])
+func nameWithLabel(name: String, age: Int) throws {
   let captureAge = Reference<Int>()
   
   let regex = Regex {
